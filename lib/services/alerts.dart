c842300cc0d652210c18bb41703b8bcfb183013c
cayan <cayanjiyan62@hotmail.com>
2022-12-09 18:43:52 +0300

    alert dialoglar adaptive hale geldi

--- lib/services/alerts.dart
+++ lib/services/alerts.dart
@@ -25,24 +25,47 @@ class CustomAlerts {
 
   static confirmActionMessage(
       BuildContext context, VoidCallback action, String actionMessage) {
-    showDialog(
-      context: context,
-      builder: (_) => CupertinoAlertDialog(
-        content: Text(
-          actionMessage,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-        ),
-        actions: [
-          CupertinoDialogAction(
-            onPressed: action,
-            child: Text(AppLocalizations.of(context)!.okay),
+    bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
+    if (isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (_) => CupertinoAlertDialog(
+          content: Text(
+            actionMessage,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
           ),
-          CupertinoDialogAction(
-            child: Text(AppLocalizations.of(context)!.cancel),
-            onPressed: () => Navigator.pop(context),
-          )
-        ],
-      ),
-    );
+          actions: [
+            CupertinoDialogAction(
+              onPressed: action,
+              child: Text(AppLocalizations.of(context)!.okay),
+            ),
+            CupertinoDialogAction(
+              child: Text(AppLocalizations.of(context)!.cancel),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          content: Text(
+            actionMessage,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          actions: [
+            TextButton(
+              onPressed: action,
+              child: Text(AppLocalizations.of(context)!.okay),
+            ),
+            TextButton(
+              child: Text(AppLocalizations.of(context)!.cancel),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      );
+    }
   }
 }
